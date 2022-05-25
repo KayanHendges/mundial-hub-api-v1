9118dea3ba43efdadd53c16fbf0a68c4d9db84b8
Kayan Hendges <kayanbhendges@gmail.com>
2022-05-25 17:07:50 -0300

    1.0

--- src/models/temp.ts
+++ src/models/temp.ts
@@ -1,127 +1,263 @@
-import { getTime } from "date-fns"
-import { Request, Response } from "express"
+import { Request, response, Response } from "express"
 import Connect from "../database/Connect"
-import Product from "../services/Products/ProductDataBase"
-import sleep from "../services/sleep"
-import titleize from "../services/titleize"
-import TrayProducts from "../services/Tray/TrayProducts"
-import OAuth2Tray from "./Auth/OAuth2Tray"
-
+import fs from 'fs';
+import TrayProducts from "../services/Tray/TrayProducts";
+import OAuth2Tray from "./Auth/OAuth2Tray";
+import ProductDataBase from "../services/Products/ProductDataBase";
+import { getTime } from "date-fns";
+import axios from "axios";
+import sleep from "../services/sleep";
 
 class Temp {
 
     async Temp(req: Request, res: Response){    
 
-        const { store_code } = req.query
-        const storeCode = parseInt(store_code as string)
-        
-        if(isNaN(storeCode)){
-            res.status(401).json({ message: 'missing store code' })
-        }
+        const errorProducts = fs.readFileSync(`./src/files/google-errors-products.txt`, 'utf8',)
+        const getCredentials = OAuth2Tray.getStoreCredentials(1049898)
+        const getwhiteListProducts = getWhiteList(errorProducts)
+        const getAllProducts = getProductsToDelete()
 
-        const products = await getProducts(storeCode)
+        const { "0": whiteList, "1": allProducts, "2": storeCredentiasl } = await Promise.all([
+            getwhiteListProducts,
+            getAllProducts,
+            getCredentials
+        ])
 
-        const newProducts = products.map( product => {
+        const productsToHandle = []
 
-            var name = product.product_name.toLowerCase()
-            
-            name.replaceAll('agrícola', '')
-            name.replaceAll('empilhadeira', '')
-            name.replaceAll('retroescavadeira', '')
-            name.replaceAll('tobata', '')
-            name.replaceAll('trator', '')
-            name.replaceAll('moto', '')
-
-            var construcaoRadial = ''
-
-            name.split(' ').map( (word, index) => {
-                
-                word.split('').map((crt, i) => {
-
-                    if(index = 1){
-                        if(crt == 'r'){
-                            construcaoRadial = 'radial'
-                        }
-                    }
-
-                })   
+        allProducts.map( product => {
+            var isWhiteProduct = false
+            whiteList.map( whiteProduct => {
+                if(product.hub_id == whiteProduct.hub_id){
+                    isWhiteProduct = true
+                }
             })
-
-            const description = `${titleize(product.product_name)}
-
-            Modelo: ${product.model}
-            Marca: ${product.brand}
-            ${construcaoRadial.length > 0? 'Construção: ': ''}${construcaoRadial.length > 0? construcaoRadial: ''}
-            
-            Dimensões:
-            
-            Comprimento: ${product.length}cm
-            Altura: ${product.height}cm
-            Largura: ${product.width}cm
-            
-            Peso: ${product.weight/1000}kg
-            garantia: 5 anos
-            `
-
-            return {...product, product_description: description.replaceAll("\n", "</br>")}
+            if(!isWhiteProduct){
+                productsToHandle.push(product)
+            }
         })
 
-        for(const i in newProducts){
-            console.log('nova operação')
-            const index = parseInt(i)
-            const product = newProducts[index]
-            const description = product.product_description as string
 
-            const start = getTime(new Date())
+        console.log(allProducts.length, productsToHandle.length, whiteList.length)
 
-            const storeCredentials = await OAuth2Tray.getStoreCredentials(storeCode)
+        await deleteLoop(productsToHandle, 0)
 
-            await Product.updateProduct({ hub_id: product.hub_id, product_description: description })
-            .catch( err => { throw new Error(err) } )
+        res.send(productsToHandle)
 
-            if(product.tray_product_id > 0){
-                await TrayProducts.updateProduct(
-                    storeCredentials,
-                    { product_description: description, is_kit: 0 },
-                    product.tray_product_id
-                )
-                .catch( err => { throw new Error(err) } )    
-            }
-
-            const end = getTime(new Date())
-            const executionTime = end - start
-            const timeRemaining = executionTime < 340? 340 - executionTime : 0
-
-            console.log(`${index+1}/${products.length} - product ${product.hub_id} - levou ${executionTime}ms e esperará ${timeRemaining} `)
-
-            await sleep(timeRemaining)
-
-            console.log('esperou, fim da operação')
-        }
-        
-        
-        res.send(newProducts)
-
-        function getProducts(storeCode: number): Promise<any[]>{
+        async function deleteLoop(list: any[], index: number): Promise<void>{
             return new Promise(async(resolve) => {
+                if(list.length > index){
+                    resolve()
+                    return
+                }
 
-                const sql = `SELECT p.hub_id, p.product_name, p.product_description, p.brand, p.model, tp.tray_product_id,
-                p.weight, p.width, p.length, p.height
-                FROM produtos p JOIN tray_produtos tp ON p.hub_id = tp.hub_id 
-                WHERE tp.tray_store_id = ${storeCode} and p.is_kit = 0`
+                const start = getTime(new Date())
+                const product = list[index]
+
+                if(!product){
+                    resolve()
+                    return
+                }
+
+                const deleted = await TrayProducts.delete(storeCredentiasl, product.tray_product_id)
+                .then(response => { return true })
+                .catch( err => { console.log('erro ao excluir', product.tray_product_id); return false } )
+
+                if(!deleted){
+                    resolve()
+                    return
+                }
+
+                const sqlCondition = `tray_store_id = 1049898 and tray_pricing_id = ${product.tray_pricing_id}`
+
+                const updated = await ProductDataBase.updatePricing({ tray_product_id: 0 }, sqlCondition)
+                .then(response => { return true })
+                .catch( err => { console.log('erro ao atualizar', product.tray_product_id); return false } )
+
+                const end = getTime(new Date())
+                const executionTime = end - start
+
+                console.log(`${Math.ceil(( ((index+1)/list.length) * 100 ))}% ${index+1}/${list.length} - concluído em ${executionTime} ms`)
+
+                setTimeout(() => {
+                    resolve(deleteLoop(list, index + 1))
+                }, 340 - executionTime)
+
+            })
+        }
+
+        async function getWhiteList(errorProducts: string): Promise<any[]>{
+            return new Promise((resolve, reject) => {
+                const sql = `SELECT *
+                FROM produtos p JOIN tray_produtos tp ON p.hub_id=tp.hub_id
+                WHERE p.is_kit = 0 AND tp.tray_store_id = 1049898 AND tray_product_id NOT IN (${errorProducts})
+                AND p.ean NOT LIKE ''`
+                
+                Connect.query(sql, (err, result) => {
     
-                Connect.query(sql, (err, res) => {
                     if(err){
                         console.log(err)
+                        reject(err)
                     } else {
-                        resolve(res)
+                        resolve(result)
                     }
                 })
+                
             })
+        }
 
+        async function getProductsToDelete(): Promise<any[]>{
+            return new Promise((resolve, reject) => {
+                const sql = `SELECT *
+                FROM produtos p JOIN tray_produtos tp ON p.hub_id=tp.hub_id
+                WHERE p.is_kit = 0 AND tp.tray_store_id = 1049898 AND tray_product_id > 0`
+                
+                Connect.query(sql, (err, result) => {
+    
+                    if(err){
+                        console.log(err)
+                        reject(err)
+                    } else {
+                        resolve(result)
+                    }
+                })
+                
+            })
         }
     }
 
+    async Temp2(req: Request, res: Response){    
+
+        const trayProducts = []
+
+        const getCredentials = OAuth2Tray.getStoreCredentials(1049898)
+        const getAllProducts = getProductsToDelete()
+
+        const { "0": allProducts, "1": storeCredentials  } = await Promise.all([
+            getAllProducts,
+            getCredentials
+        ])
+
+        await getTrayProducts(storeCredentials, 1)
+        .catch(err => { console.log(err) })
+
+        const productsToHandle = []
+
+        trayProducts.map(trayProduct => {
+            var isIncluded = false
+            allProducts.map(product => {
+                if(product.tray_product_id == trayProduct){
+                    isIncluded = true
+                }
+            })
+            if(!isIncluded){
+                productsToHandle.push(trayProduct)
+            }
+        })
+        
+        console.log(productsToHandle.length)
+
+        res.send(productsToHandle)
+
+        async function deleteLoop(list: any[], index: number): Promise<void>{
+            return new Promise(async(resolve) => {
+                if(list.length > index){
+                    resolve()
+                    return
+                }
+
+                const start = getTime(new Date())
+                const product = list[index]
+
+                if(!product){
+                    resolve()
+                    return
+                }
+
+                const deleted = await TrayProducts.delete(storeCredentials, product.tray_product_id)
+                .then(response => { return true })
+                .catch( err => { console.log('erro ao excluir', product.tray_product_id); return false } )
+
+                if(!deleted){
+                    resolve()
+                    return
+                }
+
+                const sqlCondition = `tray_store_id = 1049898 and tray_pricing_id = ${product.tray_pricing_id}`
+
+                const updated = await ProductDataBase.updatePricing({ tray_product_id: 0 }, sqlCondition)
+                .then(response => { return true })
+                .catch( err => { console.log('erro ao atualizar', product.tray_product_id); return false } )
+
+                const end = getTime(new Date())
+                const executionTime = end - start
+
+                console.log(`${Math.ceil(( ((index+1)/list.length) * 100 ))}% ${index+1}/${list.length} - concluído em ${executionTime} ms`)
+
+                setTimeout(() => {
+                    resolve(deleteLoop(list, index + 1))
+                }, 340 - executionTime)
+
+            })
+        }
+
+        async function getTrayProducts(store: any, page: number): Promise<void>{
+            return new Promise((resolve, reject) => {
+                
+                const query = `${store.api_address}/products/?page=${page}&limit=50&access_token=${store.access_token}`
+
+                const config: any = {
+                    method: 'get',
+                    url: query,
+                    headers: { 
+                        'Content-Type': 'application/json'
+                    },
+                }
+
+                axios(config)
+                .then(async(response) => {
+
+                    const products = response?.data?.Products
+                    if(products.length == 0){
+                        resolve()
+                        return
+                    }
+
+                    products?.map( product => {
+                        const id = parseInt(product.Product.id)
+                        trayProducts.push(id)
+                    })
+
+                    await sleep(340)
+
+                    resolve(getTrayProducts(store, page + 1))
+                })
+                .catch(erro => {
+                    reject(erro)
+                })
+                    
+            })
+        }
+
+        async function getProductsToDelete(): Promise<any[]>{
+            return new Promise((resolve, reject) => {
+                const sql = `SELECT *
+                FROM produtos p JOIN tray_produtos tp ON p.hub_id=tp.hub_id
+                WHERE p.is_kit = 0 AND tp.tray_store_id = 1049898 AND tray_product_id > 0`
+                
+                Connect.query(sql, (err, result) => {
+    
+                    if(err){
+                        console.log(err)
+                        reject(err)
+                    } else {
+                        resolve(result)
+                    }
+                })
+                
+            })
+        }
+    }
 }
 
 export default new Temp
