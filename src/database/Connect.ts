69c6d3dba8c7423b241f93712f148eb85f9dac5f
Kayan Hendges <kayanbhendges@gmail.com>
2022-06-02 14:50:44 -0300

    cleaning database config

--- src/database/Connect.ts
+++ src/database/Connect.ts
@@ -1,20 +1,11 @@
 import mysql from 'mysql'
 
 const Connect = mysql.createPool({
-    host: '3.136.243.230',
+    host: '',
     port: 3306,
-    user: 'prisma',
-    password: 'Db264080Db', 
-    database: 'mundialh_mundial_hub'
+    user: '',
+    password: '', 
+    database: ''
 })
-
-
-// const Connect = mysql.createPool({
-//     host: '162.240.24.27',
-//     port: 3306,
-//     user: 'mundialh_prisma',
-//     password: 'Db264080DbPrisma', 
-//     database: 'mundialh_mundial_hub'
-// })
 
 export default Connect
