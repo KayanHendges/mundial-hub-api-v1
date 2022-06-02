2a436fe439c06cc0570e4d41c22b140b08a90e5d
Kayan Hendges <kayanbhendges@gmail.com>
2022-06-02 17:29:25 -0300

    1.0

--- dist/database/Connect.js
+++ dist/database/Connect.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql_1 = __importDefault(require("mysql"));
 const Connect = mysql_1.default.createPool({
-    host: '',
-    port: 3306,
-    user: '',
-    password: '',
-    database: ''
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT),
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 exports.default = Connect;
--- src/database/Connect.ts
+++ src/database/Connect.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql'
+import env from 'dotenv/config'
 
 const Connect = mysql.createPool({
-    host: '',
-    port: 3306,
-    user: '',
-    password: '', 
-    database: ''
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT),
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD, 
+    database: process.env.MYSQL_DATABASE
 })
 
 export default Connect
