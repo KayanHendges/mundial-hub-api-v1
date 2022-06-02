9e4e5920189cfb2606c6e4508da087506164cf15
Kayan Hendges <kayanbhendges@gmail.com>
2022-06-02 14:51:35 -0300

    1.0

--- dist/database/Connect.js
+++ dist/database/Connect.js
@@ -5,17 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql_1 = __importDefault(require("mysql"));
 const Connect = mysql_1.default.createPool({
-    host: '3.136.243.230',
+    host: '',
     port: 3306,
-    user: 'prisma',
-    password: 'Db264080Db',
-    database: 'mundialh_mundial_hub'
+    user: '',
+    password: '',
+    database: ''
 });
-// const Connect = mysql.createPool({
-//     host: '162.240.24.27',
-//     port: 3306,
-//     user: 'mundialh_prisma',
-//     password: 'Db264080DbPrisma', 
-//     database: 'mundialh_mundial_hub'
-// })
 exports.default = Connect;
