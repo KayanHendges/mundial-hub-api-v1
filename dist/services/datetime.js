6c329c5cb8240ff929b600bacddba4b34a748d66
Kayan Hendges <68859964+KayanHendges@users.noreply.github.com>
2022-06-02 17:43:44 -0300

    Delete dist directory

--- dist/services/datetime.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { addHours, format } = require("date-fns");
-async function Datetime() {
-    const returnDate = format(addHours(new Date(), 0), 'yyyy-MM-dd HH:MM:SS');
-    return returnDate;
-}
-exports.default = Datetime;
